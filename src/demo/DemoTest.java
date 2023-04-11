57fed9c6c7eb597615f143667f5f00d5718c38e9
meno <meno@menos-mbp.hsd1.mi.comcast.net>
2023-04-10 22:45:27 -0400

    NEW

--- src/demo/DemoTest.java
+++ src/demo/DemoTest.java
@@ -5,15 +5,9 @@ public class DemoTest {
 	public static void main(String[] args) {
 		
 		System.out.println("Team...");
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 		System.out.println("pegah");
-=======
-<<<<<<< HEAD
-=======
 
-
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/B9-Team2.git
 		
 		String name2 ="Tina";
 
@@ -24,32 +18,20 @@ public class DemoTest {
 
 		System.out.println("karzan");
 		System.out.println("Wirya");
-<<<<<<< HEAD
+
 		System.out.println("hyyyyy");
-//=======
-//<<<<<<< HEAD
-=======
 
-
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/B9-Team2.git
 		
 		String firstName = "Naz";
 
 		
 		String car = "toyota";
 		
-		
 		System.out.println("Luis");
-<<<<<<< HEAD
-		//test
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/B9-Team2.git
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/B9-Team2.git
+
 	System.out.println("pegahhhh");
 		System.out.println("pegah");
-=======
 
-//hii
 
->>>>>>> branch 'master' of https://github.com/noorteck-sdet/B9-Team2.git
 	}
 }
